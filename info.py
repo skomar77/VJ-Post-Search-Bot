e95a48cd3483f9368b7e964413ed9e9392ba5eb1
skomar77 <balkdik99@gmail.com>
2024-12-23 17:29:34 +0600

    Update info.py
    info.py

--- info.py
+++ info.py
@@ -1,11 +1,3 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
-# Never Give DATABASE_URI And BOT_TOKEN Here If Repo Is Forked Or Public Because This May Lead Account Ban.
-
-import os
-from os import environ
 
 API_ID       = int(environ.get("API_ID", ""))
 API_HASH     = environ.get("API_HASH", "")
